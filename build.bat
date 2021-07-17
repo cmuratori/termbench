218af223d485bebf41ea4d022c2b892953a9f3f6
Casey Muratori <casey@mollyrocket.com>
2021-07-17 13:14:24 -0700

    Changed architecture to be test-focused; removed unnecessary Win32 console calls

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 
 set BaseFile=termbench.cpp
-set CLLinkFlags=-nodefaultlib -incremental:no -opt:ref -machine:x64 -STACK:0x100000,0x100000 -manifest:no -subsystem:console kernel32.lib
-set CLCompileFlags=-Zi -d2Zi+ -Gy -GF -Gs9999999 -GS- -GR- -EHs- -EHc- -EHa- -WX -W4 -nologo -FC -Gm- -diagnostics:column -fp:except- -fp:fast
-set CLANGCompileFlags= -g -fno-autolink -nostdlib -nostdlib++ -mno-stack-arg-probe
-set CLANGLinkFlags=-fuse-ld=lld -Wl,-subsystem:console,kernel32.lib
+set CLLinkFlags=-nodefaultlib -incremental:no -opt:ref -machine:x64 -STACK:0x100000,0x100000 -manifest:no -subsystem:console kernel32.lib user32.lib
+set CLCompileFlags=-Zi -Zl -d2Zi+ -Gy -GF -Gs9999999 -GS- -GR- -EHs- -EHc- -EHa- -WX -W4 -nologo -FC -Gm- -diagnostics:column -fp:except- -fp:fast
+set CLANGCompileFlags= -D_VC_NODEFAULTLIB=1 -g -fno-autolink -nostdlib -nostdlib++ -mno-stack-arg-probe
+set CLANGLinkFlags=-fuse-ld=lld -Wl,-subsystem:console,kernel32.lib,user32.lib
 
 echo -----------------
 echo Building debug:
--- termbench.cpp
+++ termbench.cpp
@@ -1,4 +1,4 @@
-#define VERSION_NAME "TermMarkV1"
+#define VERSION_NAME "TermMarkV2"
 
 #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
 
@@ -21,27 +21,26 @@ static void AppendString(buffer *Buffer, char const *String)
     while(*String) AppendChar(Buffer, *String++);
 }
 
-static void AppendDecimal(buffer *Buffer, int Value)
+static void AppendDecimal(buffer *Buffer, int unsigned Value)
 {
-    if(Value < 0)
-    {
-        AppendChar(Buffer, '-');
-        Value = -Value;
-    }
-    
-    int Remains = Value;
+    int unsigned Remains = Value;
     for(int Divisor = 1000000000;
         Divisor > 0;
         Divisor /= 10)
     {
         int Digit = Remains / Divisor;
         Remains -= Digit*Divisor;
-        
+
         if(Digit || (Value != Remains) || (Divisor == 1))
         {
             AppendChar(Buffer, (char)('0' + Digit));
         }
     }
+}
+
+static void AppendDouble(buffer *Buffer, double Value)
+{
+    AppendDecimal(Buffer, (int unsigned)(Value * 1000.0));
 }
 
 static void AppendGoto(buffer *Buffer, int X, int Y)
@@ -64,19 +63,11 @@ static void AppendColor(buffer *Buffer, int IsForeground, int unsigned Red, int 
     AppendChar(Buffer, 'm');
 }
 
-static int GetMS(int long long Start, int long long End, int long long Frequency)
+static double GetGBS(double Bytes, double Seconds)
 {
-    int Result = (int)(1000*(End - Start) / Frequency);
+    double BytesPerGigabyte = 1024.0*1024.0*1024.0;
+    double Result = Bytes / (BytesPerGigabyte*Seconds);
     return Result;
-}
-
-static void AppendStat(buffer *Buffer, char const *Name, int Value, char const *Suffix = "")
-{
-    AppendString(Buffer, Name);
-    AppendString(Buffer, ": ");
-    AppendDecimal(Buffer, Value);
-    AppendString(Buffer, Suffix);
-    AppendString(Buffer, "  ");
 }
 
 #define MAX_TERM_WIDTH 4096
@@ -85,230 +76,194 @@ static char TerminalBuffer[256+16*MAX_TERM_WIDTH*MAX_TERM_HEIGHT];
 
 #include <windows.h>
 #include <intrin.h>
+#include "fast_pipe.h"
+
+struct test_context
+{
+    HANDLE TerminalOut;
+    buffer Frame;
+    int Width;
+    int Height;
+    size_t TestCount;
+
+    size_t TotalWriteCount;
+    double SecondsElapsed;
+};
+
+static void RawFlushBuffer(HANDLE TerminalOut, buffer *Frame)
+{
+    WriteFile(TerminalOut, Frame->Data, Frame->Count, 0, 0);
+    Frame->Count = 0;
+}
+
+static void FlushBuffer(test_context *Test, buffer *Frame)
+{
+    Test->TotalWriteCount += Frame->Count;
+    RawFlushBuffer(Test->TerminalOut, Frame);
+}
+
+static void FGBGPerChar(test_context *Test)
+{
+    buffer Frame = Test->Frame;
+
+    for(int FrameIndex = 0; FrameIndex < Test->TestCount; ++FrameIndex)
+    {
+        for(int Y = 0; Y <= Test->Height; ++Y)
+        {
+            AppendGoto(&Frame, 1, 1 + Y);
+            for(int X = 0; X <= Test->Width; ++X)
+            {
+                int BackRed = FrameIndex + Y + X;
+                int BackGreen = FrameIndex + Y;
+                int BackBlue = FrameIndex;
+
+                int ForeRed = FrameIndex;
+                int ForeGreen = FrameIndex + Y;
+                int ForeBlue = FrameIndex + Y + X;
+
+                AppendColor(&Frame, false, BackRed, BackGreen, BackBlue);
+                AppendColor(&Frame, true, ForeRed, ForeGreen, ForeBlue);
+
+                char Char = 'a' + (char)((FrameIndex + X + Y) % ('z' - 'a'));
+                AppendChar(&Frame, Char);
+            }
+        }
+
+        FlushBuffer(Test, &Frame);
+    }
+}
+
+typedef void test_function(test_context *Test);
+enum test_size
+{
+    TestSize_Normal,
+
+    TestSize_Small,
+    TestSize_Large,
+
+    TestSize_Count,
+};
+struct test
+{
+    char const *Name;
+    test_function *Function;
+    size_t TestCount[TestSize_Count];
+};
+
+static test Tests[] =
+{
+    {"FGBGPerChar", FGBGPerChar, {64, 1024, 65536}},
+};
 
 extern "C" void mainCRTStartup(void)
 {
+    int BypassConhost = USE_FAST_PIPE_IF_AVAILABLE();
+    int VirtualTerminalSupport = 0;
+    int TestSize = TestSize_Large;
+
     char CPU[65] = {};
-    for(int SegmentIndex = 0;
-        SegmentIndex < 3;
-        ++SegmentIndex)
+    for(int SegmentIndex = 0; SegmentIndex < 3; ++SegmentIndex)
     {
         __cpuid((int *)(CPU + 16*SegmentIndex), 0x80000002 + SegmentIndex);
     }
-    
+
     for(int Num = 0; Num < 256; ++Num)
     {
         buffer NumBuf = {sizeof(NumberTable[Num]), 0, NumberTable[Num]};
         AppendDecimal(&NumBuf, Num);
         AppendChar(&NumBuf, 0);
     }
-    
-    HANDLE TerminalIn = GetStdHandle(STD_INPUT_HANDLE);
+
     HANDLE TerminalOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    
-    DWORD WinConMode = 0;
-    DWORD EnableVirtualTerminalProcessing = 0x0004;
-    int VirtualTerminalSupport = (GetConsoleMode(TerminalOut, &WinConMode) &&
-                                  SetConsoleMode(TerminalOut, (WinConMode & ~(ENABLE_ECHO_INPUT|ENABLE_LINE_INPUT)) |
-                                                 EnableVirtualTerminalProcessing));
-    
+
+#if _WIN32
+    if(!BypassConhost)
+    {
+        DWORD WinConMode = 0;
+        DWORD EnableVirtualTerminalProcessing = 0x0004;
+         VirtualTerminalSupport = (GetConsoleMode(TerminalOut, &WinConMode) &&
+                                   SetConsoleMode(TerminalOut, (WinConMode & ~(ENABLE_ECHO_INPUT|ENABLE_LINE_INPUT)) |
+                                                  EnableVirtualTerminalProcessing));
+    }
+#endif
+
     LARGE_INTEGER Freq;
     QueryPerformanceFrequency(&Freq);
-    
-    int PrepMS = 0;
-    int WriteMS = 0;
-    int ReadMS = 0;
-    int TotalMS = 0;
-    
-    int Running = true;
-    int FrameIndex = 0;
-    int DimIsKnown = false;
-    
-    int WritePerLine = false;
-    int ColorPerFrame = false;
-    
-    int Width = 0;
-    int Height = 0;
-    
-    int ByteCount = 0;
-    int TermMark = 0;
-    int StatPercent = 0;
-    
-    LARGE_INTEGER AverageMark = {};
-    int long long TermMarkAccum = 0;
-    
-    while(Running)
+
+    int Width = 80;
+    int Height = 24;
+
+    if(Width > MAX_TERM_WIDTH) Width = MAX_TERM_WIDTH;
+    if(Height > MAX_TERM_HEIGHT) Height = MAX_TERM_HEIGHT;
+
+    buffer Frame = {sizeof(TerminalBuffer), 0, TerminalBuffer};
+
+    test_context Contexts[ArrayCount(Tests)] = {};
+    for(int TestIndex = 0; TestIndex < ArrayCount(Tests); ++TestIndex)
     {
-        int NextByteCount = 0;
-        
-        LARGE_INTEGER A;
-        QueryPerformanceCounter(&A);
-        
-        if(TermMarkAccum == 0)
-        {
-            AverageMark = A;
-        }
-        else
-        {
-            int long long AvgMS = 1000*(A.QuadPart - AverageMark.QuadPart) / Freq.QuadPart;
-            int long long StatMS = 10000;
-            if(AvgMS > StatMS)
-            {
-                TermMark = (int)(1000*(TermMarkAccum / 1024) / AvgMS);
-                AverageMark = A;
-                TermMarkAccum = 0;
-            }
-            
-            StatPercent = (int)(100*AvgMS / StatMS);
-        }
-        
-        if(!DimIsKnown)
-        {
-            CONSOLE_SCREEN_BUFFER_INFO ConsoleInfo;
-            GetConsoleScreenBufferInfo(TerminalOut, &ConsoleInfo);
-            Width = ConsoleInfo.srWindow.Right - ConsoleInfo.srWindow.Left;
-            Height = ConsoleInfo.srWindow.Bottom - ConsoleInfo.srWindow.Top;
-            DimIsKnown = true;
-        }
-        
-        if(Width > MAX_TERM_WIDTH) Width = MAX_TERM_WIDTH;
-        if(Height > MAX_TERM_HEIGHT) Height = MAX_TERM_HEIGHT;
-        
-        buffer Frame = {sizeof(TerminalBuffer), 0, TerminalBuffer};
-        
-        for(int Y = 0; Y <= Height; ++Y)
-        {
-            AppendGoto(&Frame, 1, 1 + Y);
-            for(int X = 0; X <= Width; ++X)
-            {
-                if(!ColorPerFrame)
-                {
-                    int BackRed = FrameIndex + Y + X;
-                    int BackGreen = FrameIndex + Y;
-                    int BackBlue = FrameIndex;
-                    
-                    int ForeRed = FrameIndex;
-                    int ForeGreen = FrameIndex + Y;
-                    int ForeBlue = FrameIndex + Y + X;
-                    
-                    AppendColor(&Frame, false, BackRed, BackGreen, BackBlue);
-                    AppendColor(&Frame, true, ForeRed, ForeGreen, ForeBlue);
-                }
-                
-                char Char = 'a' + (char)((FrameIndex + X + Y) % ('z' - 'a'));
-                AppendChar(&Frame, Char);
-            }
-        
-            if(WritePerLine)
-            {
-                NextByteCount += Frame.Count;
-                WriteConsoleA(TerminalOut, Frame.Data, Frame.Count, 0, 0);
-                Frame.Count = 0;
-            }
-        }
-        
-        AppendColor(&Frame, false, 0, 0, 0);
-        AppendColor(&Frame, true, 255, 255, 255);
-        AppendGoto(&Frame, 1, 1);
-        AppendStat(&Frame, "Glyphs", (Width*Height) / 1024, "k");
-        AppendStat(&Frame, "Bytes", ByteCount / 1024, "kb");
-        AppendStat(&Frame, "Frame", FrameIndex);
+        test Test = Tests[TestIndex];
 
-        if(!WritePerLine) AppendStat(&Frame, "Prep", PrepMS, "ms");
-        AppendStat(&Frame, "Write", WriteMS, "ms");
-        AppendStat(&Frame, "Read", ReadMS, "ms");
-        AppendStat(&Frame, "Total", TotalMS, "ms");
-        
-        AppendGoto(&Frame, 1, 2);
-        AppendString(&Frame, WritePerLine ? "[F1]:write per line " : "[F1]:write per frame ");
-        AppendString(&Frame, ColorPerFrame ? "[F2]:color per frame " : "[F2]:color per char ");
-        
-        if(!WritePerLine)
-        {
-            AppendGoto(&Frame, 1, 3);
-            if(TermMark)
-            {
-                AppendStat(&Frame, VERSION_NAME, TermMark, ColorPerFrame ? "kg/s" : "kcg/s");
-                AppendString(&Frame, "(");
-                AppendString(&Frame, CPU);
-                AppendString(&Frame, " Win32 ");
-                AppendString(&Frame, VirtualTerminalSupport ? "VTS)" : "NO VTS REPORTED)");
-            }
-            else
-            {
-                AppendStat(&Frame, "(collecting", StatPercent, "%)");
-            }
-        }
-        
-        LARGE_INTEGER B;
-        QueryPerformanceCounter(&B);
-        
-        NextByteCount += Frame.Count;
-        WriteConsoleA(TerminalOut, Frame.Data, Frame.Count, 0, 0);
-        
-        LARGE_INTEGER C;
-        QueryPerformanceCounter(&C);
+        test_context *Context = Contexts + TestIndex;
+        Context->TerminalOut = TerminalOut;
+        Context->Frame = Frame;
+        Context->Width = Width;
+        Context->Height = Height;
+        Context->TestCount = Test.TestCount[TestSize];
 
-        int ResetStats = false;
-        while(WaitForSingleObject(TerminalIn, 0) == WAIT_OBJECT_0)
-        {
-            INPUT_RECORD Record;
-            DWORD RecordCount = 0;
-            ReadConsoleInput(TerminalIn, &Record, 1, &RecordCount);
-            if(RecordCount)
-            {
-                if((Record.EventType == KEY_EVENT) &&
-                   (Record.Event.KeyEvent.bKeyDown) &&
-                   (Record.Event.KeyEvent.wRepeatCount == 1))
-                {
-                    switch(Record.Event.KeyEvent.wVirtualKeyCode)
-                    {
-                        case VK_ESCAPE: Running = false; break;
-                        case VK_F1:
-                        {
-                            WritePerLine = !WritePerLine;
-                            ResetStats = true;
-                        } break;
-                        
-                        case VK_F2:
-                        {
-                            ColorPerFrame = !ColorPerFrame;
-                            ResetStats = true;
-                        } break;
-                    }
-                }
-                else if(Record.EventType == WINDOW_BUFFER_SIZE_EVENT)
-                {
-                    DimIsKnown = false;
-                    ResetStats = true;
-                }
-            }
-        }
-        
-        LARGE_INTEGER D;
-        QueryPerformanceCounter(&D);
-        
-        PrepMS = GetMS(A.QuadPart, B.QuadPart, Freq.QuadPart);
-        WriteMS = GetMS(B.QuadPart, C.QuadPart, Freq.QuadPart);
-        ReadMS = GetMS(C.QuadPart, D.QuadPart, Freq.QuadPart);
-        TotalMS = GetMS(A.QuadPart, D.QuadPart, Freq.QuadPart);
-        ByteCount = NextByteCount;
-        ++FrameIndex;
-        
-        if(ResetStats)
-        {
-            TermMarkAccum = TermMark = 0;
-        }
-        else
-        {
-            TermMarkAccum += Width*Height;
-        }
+        LARGE_INTEGER Start;
+        QueryPerformanceCounter(&Start);
+        Test.Function(Context);
+        LARGE_INTEGER End;
+        QueryPerformanceCounter(&End);
+
+        Context->SecondsElapsed = (double)(End.QuadPart - Start.QuadPart) / (double)Freq.QuadPart;
     }
+
+    AppendGoto(&Frame, 1, 1);
+    AppendColor(&Frame, 0, 0, 0, 0);
+    AppendColor(&Frame, 1, 255, 255, 255);
+    AppendString(&Frame, "\x1b[0m");
+
+    AppendString(&Frame, "CPU: ");
+    AppendString(&Frame, CPU);
+    AppendString(&Frame, "\n");
+    AppendString(&Frame, "VT support: ");
+    AppendString(&Frame, VirtualTerminalSupport ? "yes" : "no");
+    AppendString(&Frame, "\n");
+
+    double TotalSeconds = 0.0;
+    size_t TotalBytes = 0;
+    for(int TestIndex = 0; TestIndex < ArrayCount(Tests); ++TestIndex)
+    {
+        test Test = Tests[TestIndex];
+        test_context Context = Contexts[TestIndex];
+
+        AppendString(&Frame, Test.Name);
+        AppendString(&Frame, ": ");
+        AppendDouble(&Frame, Context.SecondsElapsed);
+        AppendString(&Frame, "s (");
+        AppendDouble(&Frame, GetGBS((double)Context.TotalWriteCount, Context.SecondsElapsed));
+        AppendString(&Frame, "gb/s)\n");
+
+        TotalSeconds += Context.SecondsElapsed;
+        TotalBytes += Context.TotalWriteCount;
+    }
+
+    AppendString(&Frame, VERSION_NAME);
+    AppendString(&Frame, ": ");
+    AppendDouble(&Frame, TotalSeconds);
+    AppendString(&Frame, "s (");
+    AppendDouble(&Frame, GetGBS((double)TotalBytes, TotalSeconds));
+    AppendString(&Frame, "gb/s)\n");
+
+    RawFlushBuffer(TerminalOut, &Frame);
 }
 
 //
 // NOTE(casey): Support definitions for CRT-less Visual Studio and CLANG
 //
+
+extern "C" int _fltused = 0;
 
 #ifndef __clang__
 #undef function
@@ -318,10 +273,10 @@ extern "C" void *memset(void *DestInit, int Source, size_t Size)
 {
     unsigned char *Dest = (unsigned char *)DestInit;
     while(Size--) *Dest++ = (unsigned char)Source;
-    
+
     return(DestInit);
 }
-    
+
 #ifndef __clang__
 #pragma function(memcpy)
 #endif
@@ -330,6 +285,6 @@ extern "C" void *memcpy(void *DestInit, void const *SourceInit, size_t Size)
     unsigned char *Source = (unsigned char *)SourceInit;
     unsigned char *Dest = (unsigned char *)DestInit;
     while(Size--) *Dest++ = *Source++;
-    
+
     return(DestInit);
 }
